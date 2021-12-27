a3a016054fed36fb9249f7e1f3dc233b4d2b1d44
Zishan <zishan@inurture.co.in>
2021-12-27 13:37:13 +0530

    variable, use strict, variable naming convention added.

---
+++ Part1-Basics/variablenaming.js
@@ -0,0 +1,16 @@
+// Variable naming convention.
+
+var 1fruit = 'Apple'
+console.log(1fruit); //Invalid variable name format, will throw error.
+
+var fruit2 ='Mango' //Valid variable name
+
+var _flower = 'Rose'; //Valid
+
+var $flower ='Rose';  // Valid
+
+var #flower = 'Rose';  //Invalid variable name format, will throw error.
+
+var flower_name = 'Rose' //Valid
+
+var flower-name = 'Rose' //Invalid variable name format, will throw error.
--- Part1-Basics/variables.js
+++ Part1-Basics/variables.js
@@ -1,4 +1,32 @@
+'use strict';
+
+//In this chapter we will learn, Variable, the "use strict" directive.
+
 var firstName = "Zishan";
 var siblings = 2;
 console.log(firstName);
+firstName= 'Muneer'
+console.log(firstName)
+
 console.log(siblings)
+
+
+/* The 'use strict' directive is a literal added in ES5.  
+ * The purpose of this literal is to run js code in strict mode.
+ * With strict mode we can not use undeclared variables. See the below example.
+ * We can write literal on the top of all js code.
+*/
+
+
+Age = 40
+console.log(Age)
+
+age = 30
+
+console.log(age)
+
+
+
+
+
+
